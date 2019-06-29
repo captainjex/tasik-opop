80f20ad0f00cbb2ccd5bb604544616ee7e53a118
wkwksama <mailpakgasing@gmail.com>
2019-06-29 15:12:37 +0700

    halaman utama

---
+++ constants/menus.js
@@ -0,0 +1,7 @@
+export const indexMenus = [
+  { icon: 'fas fa-shopping-bag', color: 'green', path: 'kerajinan', name: 'Kerajinan' },
+  { icon: 'fas fa-utensils', color: 'green', path: 'makanan', name: 'Makanan' },
+  { icon: 'fas fa-tshirt', color: 'green', path: 'fashion', name: 'Fashion' },
+  { icon: 'fas fa-street-view', color: 'green', path: 'wisata', name: 'Wisata' },
+  { icon: 'fas fa-book-open', color: 'green', path: 'buku', name: 'Buku' }
+]
--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,37 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
+  <v-layout align-center justify-center>
+    <v-flex>
+      <div class="primary--text text-xs-center">
+        <div class="display-1">
+          One Place
+        </div>
+        <div class="display-1">
+          One Product
+        </div>
+        <div class="secondary--text headline">
+          Kota Tasikmalaya
+        </div>
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >
-            Continue
+
+      <v-layout row wrap justify-center class="mt-4">
+        <v-flex v-for="(menu, index) in indexMenus" :key="index" xs6 class="text-xs-center my-0 py-0">
+          <v-btn icon large dark :color="menu.color" fab>
+            <v-icon large v-text="menu.icon" />
           </v-btn>
-        </v-card-actions>
-      </v-card>
+          <p>{{ menu.name }}</p>
+        </v-flex>
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import { indexMenus } from '../constants/menus'
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      indexMenus
+    }
   }
 }
 </script>
