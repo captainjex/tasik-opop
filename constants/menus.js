48d5f1f9d72f87ae5d4b3746600a5d9b6128b39b
wkwksama <mailpakgasing@gmail.com>
2019-06-29 23:58:13 +0700

    fix some

--- constants/menus.js
+++ constants/menus.js
@@ -1,7 +1,7 @@
 export const indexMenus = [
-  { icon: 'fas fa-shopping-bag', color: '#d64550', path: 'kerajinan', name: 'Kerajinan' },
-  { icon: 'fas fa-utensils', color: '#ffc09f', path: 'makanan', name: 'Makanan' },
-  { icon: 'fas fa-tshirt', color: '#77cbb9', path: 'fashion', name: 'Fashion' },
-  { icon: 'fas fa-street-view', color: '#6369d1', path: 'wisata', name: 'Wisata' },
-  { icon: 'fas fa-book-open', color: '#f76f8e', path: 'buku', name: 'Buku' }
+  { path: 'kerajinan', name: 'Kerajinan', icon: 'fas fa-shopping-bag', color: '#d64550' },
+  { path: 'makanan', name: 'Makanan', icon: 'fas fa-utensils', color: '#ffc09f' },
+  { path: 'fashion', name: 'Fashion', icon: 'fas fa-tshirt', color: '#77cbb9' },
+  { path: 'wisata', name: 'Wisata', icon: 'fas fa-street-view', color: '#6369d1' },
+  { path: 'buku', name: 'Buku', icon: 'fas fa-book-open', color: '#f76f8e' }
 ]
