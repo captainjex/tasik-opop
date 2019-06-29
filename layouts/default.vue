29ecec4811c2b34cc4a09715a05a515ec575e127
wkwksama <mailpakgasing@gmail.com>
2019-06-29 13:42:49 +0700

    clean the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,6 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
       app
     >
@@ -25,34 +23,15 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
-      :clipped-left="clipped"
+      v-if="false"
       fixed
       app
     >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn
         icon
-        @click.stop="rightDrawer = !rightDrawer"
+        @click.stop="drawer = !drawer"
       >
         <v-icon>menu</v-icon>
       </v-btn>
@@ -62,26 +41,10 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>
-              compare_arrows
-            </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :fixed="fixed"
       app
+      absolute
+      inset
     >
       <span>&copy; 2019</span>
     </v-footer>
@@ -92,9 +55,8 @@
 export default {
   data() {
     return {
-      clipped: false,
+      title: 'Vuetify.js',
       drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'apps',
@@ -106,11 +68,7 @@ export default {
           title: 'Inspire',
           to: '/inspire'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
     }
   }
 }
